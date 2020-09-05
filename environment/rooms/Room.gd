a4eddc9a1364ef16187fb4cd23267c08022b7ce7
mewore <46766863+mewore@users.noreply.github.com>
2020-09-05 23:04:56 +0300

    Make the room open its "doors" when the enemies are dead

--- environment/rooms/Room.gd
+++ environment/rooms/Room.gd
@@ -5,6 +5,10 @@ var LOG: Log = LogManager.get_log(self)
 onready var ENEMY_CONTAINER: Node = $Enemies
 onready var enemies_left: int = ENEMY_CONTAINER.get_child_count()
 
+onready var COMMON_MAP: TileMap = $CommonMap
+onready var DOOR_TILES: PoolVector2Array = PoolVector2Array(
+    COMMON_MAP.get_used_cells_by_id(COMMON_MAP.tile_set.find_tile_by_name("door")))
+
 func _ready() -> void:
     LOG.info("Enemy count: %d" % [enemies_left])
     for _enemy in ENEMY_CONTAINER.get_children():
@@ -12,12 +16,21 @@ func _ready() -> void:
         var enemy: Enemy = _enemy
         LOG.check_error_code(enemy.connect("dead", self, "_on_enemy_dead"),
             "Connecting an enemy's 'dead' signal to the room's '_on_enemy_dead' method")
+    
+    # TODO: Replace this with actual doors instead of walls
+    var wall_tile_id: int = COMMON_MAP.tile_set.find_tile_by_name("wall")
+    for _tile in DOOR_TILES:
+        var tile: Vector2 = _tile
+        COMMON_MAP.set_cell(tile.x, tile.y, wall_tile_id)
+        
+    
 
 func _on_enemy_dead() -> void:
     enemies_left -= 1
     LOG.info("Enemy died. Enemies left: %d" % enemies_left)
     if enemies_left <= 0:
         destroy_bullets()
+        open_doors()
 
 func destroy_bullets() -> void:
     for _bullet in get_tree().get_nodes_in_group("enemy_bullets"):
@@ -25,4 +38,6 @@ func destroy_bullets() -> void:
         bullet.disappear()
 
 func open_doors() -> void:
-    pass
+    for _tile in DOOR_TILES:
+        var tile: Vector2 = _tile
+        COMMON_MAP.set_cell(tile.x, tile.y, -1)
